59adbd781e8d3726dd03e5b9ff8a4d3496c30efd
FernandoDCYT <1001.30325234.ucla@gmail.com>
2024-10-10 10:37:33 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-import Cl_estudiante from "./Cl_estudiante.js";
-import Cl_becado from "./Cl_becado.js";
+import Cl_becado from "./Cl_becado.js"
 
 let beca1 = new Cl_becado("Mary", 132, 8);
 let beca2 = new Cl_becado("Alirio", 315, 35);
@@ -10,15 +9,15 @@ beca2.calcBeca();
 let salida = document.getElementById("salida");
 
 salida.innerHTML = `
-    Nombre del estudiante: ${est1.nombre}<br>
-    Acumulado en notas: ${est1.notAc}<br>
-    Cantidad de materias: ${est1.mat}<br>
+    Nombre del estudiante: ${beca1.nombre}<br>
+    Acumulado en notas: ${beca1.notAc}<br>
+    Cantidad de materias: ${beca1.mat}<br>
     Nota promedio: ${beca1.calcProm().toFixed(2)}<br>
     ${beca1.calcBeca()}
     <br><br>
-    Nombre del estudiante: ${est2.nombre}<br>
-    Acumulado en notas: ${est2.notAc}<br>
-    Cantidad de materias: ${est2.mat}<br>
+    Nombre del estudiante: ${beca2.nombre}<br>
+    Acumulado en notas: ${beca2.notAc}<br>
+    Cantidad de materias: ${beca2.mat}<br>
     Nota promedio: ${beca2.calcProm().toFixed(2)}<br>
     ${beca2.calcBeca()}
 `;
